fa9c85b94729a12db166420f8c394f1fb8af8749
arcdeveloper7 <91615795+arcdeveloper7@users.noreply.github.com>
2022-04-03 18:18:05 +0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,26 +1,26 @@
 clear
 echo "installing package [1/7]..."
-echo "" apt update -y > package
+echo "" apt update > package
 clear
 
 echo "installing package [2/7]..."
-echo "" apt install python -y > package
+echo "" apt install python > package
 clear
 
 echo "installing package [3/7]..."
-echo "" apt install python2 -y > package
+echo "" apt install python2 > package
 clear
 
 echo "installing package [4/7]..."
-echo "" apt install git -y > package
+echo "" apt install git > package
 clear
 
 echo "installing package [5/7]..."
-echo "" apt install ncurses-utils -y > package
+echo "" apt install ncurses-utils > package
 clear
 
 echo "installing package [6/7]..."
-echo "" apt install iproute2 -y > package
+echo "" apt install iproute2 > package
 clear
 
 echo "installing package [7/7]..."
