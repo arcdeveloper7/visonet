a4fd008a74ec44d8a544551329447608c5e6f405
arcdeveloper7 <91615795+arcdeveloper7@users.noreply.github.com>
2022-04-03 18:16:54 +0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,32 +1,32 @@
 clear
-echo installing package [1/7]...
-echo apt update -y "" > package
+echo "installing package [1/7]..."
+echo "" apt update -y > package
 clear
 
-echo installing package [2/7]...
-echo apt install python -y "" > package
+echo "installing package [2/7]..."
+echo "" apt install python -y > package
 clear
 
-echo installing package [3/7]...
-echo apt install python2 -y "" > package
+echo "installing package [3/7]..."
+echo "" apt install python2 -y > package
 clear
 
-echo installing package [4/7]...
-echo apt install git -y "" > package
+echo "installing package [4/7]..."
+echo "" apt install git -y > package
 clear
 
-echo installing package [5/7]...
-echo apt install ncurses-utils -y "" > package
+echo "installing package [5/7]..."
+echo "" apt install ncurses-utils -y > package
 clear
 
-echo installing package [6/7]...
-echo apt install iproute2 -y "" > package
+echo "installing package [6/7]..."
+echo "" apt install iproute2 -y > package
 clear
 
-echo installing package [7/7]...
-echo apt install wireless-tools "" > package
+echo "installing package [7/7]..."
+echo "" apt install wireless-tools > package
 clear
 
-echo install complete.
+echo "install complete."
 
 
